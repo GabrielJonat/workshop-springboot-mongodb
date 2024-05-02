6e1717ec85aab5b330670bd7c5e3f3d3622a47ff
Gabriel Jonathan <gabrielpadawan912@gmail.com>
2024-05-02 19:50:36 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/gabrieljonathan/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrieljonathan/workshopmongo/resources/UserResource.java
@@ -1,27 +1,27 @@
-package com.gabrieljonathan.workshopmongo.resources;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.gabrieljonathan.workshopmongo.domain.User;
-
-@RestController
-@RequestMapping(value= "/users")
-public class UserResource {
+	package com.gabrieljonathan.workshopmongo.resources;
 	
-	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		User maria  = new User("1", "Maria da Silva", "maria@gmail.com");
-		User abner  = new User("1", "Abner Carvalho", "aCarvalho912@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,abner));
-		return ResponseEntity.ok().body(list);
+	import java.util.List;
+	
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.http.ResponseEntity;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RestController;
+	
+	import com.gabrieljonathan.workshopmongo.domain.User;
+	import com.gabrieljonathan.workshopmongo.services.UserService;
+	
+	@RestController
+	@RequestMapping(value= "/users")
+	public class UserResource {
+		
+	@Autowired
+	private UserService service;
+	
+		@GetMapping
+		public ResponseEntity<List<User>> findAll(){
+			List<User> list = service.findAll();
+			return ResponseEntity.ok().body(list);
+		}
+	
 	}
-
-}
