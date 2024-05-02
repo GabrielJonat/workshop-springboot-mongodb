b1e4cb03a353b86f33bf9487803197e4a0876844
Gabriel Jonathan <gabrielpadawan912@gmail.com>
2024-05-02 18:58:34 -0300

    Projeto Criado

---
+++ src/test/java/com/gabrieljonathan/workshopmongo/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieljonathan.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
