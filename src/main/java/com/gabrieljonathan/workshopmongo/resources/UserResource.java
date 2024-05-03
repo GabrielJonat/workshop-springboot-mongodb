1358ced4fe356291f878bf669570130811c34096
Gabriel Jonathan <gabrielpadawan912@gmail.com>
2024-05-02 21:27:02 -0300

    DTO

--- src/main/java/com/gabrieljonathan/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrieljonathan/workshopmongo/resources/UserResource.java
@@ -1,15 +1,17 @@
 	package com.gabrieljonathan.workshopmongo.resources;
 	
 	import java.util.List;
-	
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.http.ResponseEntity;
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RestController;
-	
-	import com.gabrieljonathan.workshopmongo.domain.User;
-	import com.gabrieljonathan.workshopmongo.services.UserService;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrieljonathan.workshopmongo.domain.User;
+import com.gabrieljonathan.workshopmongo.dto.UserDTO;
+import com.gabrieljonathan.workshopmongo.services.UserService;
 	
 	@RestController
 	@RequestMapping(value= "/users")
@@ -19,9 +21,10 @@
 	private UserService service;
 	
 		@GetMapping
-		public ResponseEntity<List<User>> findAll(){
+		public ResponseEntity<List<UserDTO>> findAll(){
 			List<User> list = service.findAll();
-			return ResponseEntity.ok().body(list);
+			List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
 		}
 	
 	}
