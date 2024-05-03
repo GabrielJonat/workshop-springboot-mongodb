add4799dc361e557d9331de9c968de3f263c0b93
Gabriel Jonathan <gabrielpadawan912@gmail.com>
2024-05-03 02:24:49 -0300

    fullsearch

--- src/main/java/com/gabrieljonathan/workshopmongo/services/PostService.java
+++ src/main/java/com/gabrieljonathan/workshopmongo/services/PostService.java
@@ -1,13 +1,19 @@
 package com.gabrieljonathan.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.gabrieljonathan.workshopmongo.domain.Post;
 import com.gabrieljonathan.workshopmongo.repositories.PostRepository;
+import com.gabrieljonathan.workshopmongo.resources.util.URL;
 import com.gabrieljonathan.workshopmongo.services.exceptions.ObjectNotFounndException;
 
 @Service
@@ -26,8 +32,12 @@ public class PostService {
 			return post;
 	}
 	
+	public List<Post> ultraSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.UltraMegaBlasterPowerSearch(text, minDate, maxDate);
+	}
+	
 	public List<Post> findByTitle(String text){
-		
 		return repo.searchTitle(text);
 	}
 	
